e62a616381b7cae6709ae153998d2ec648870c47
Vít Čermák <v.cermak.v@gmail.com>
2020-10-15 20:46:48 +0200

    panel pic upload.

--- myproject1/urls.py
+++ myproject1/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('main.urls')),
     url(r'^news/', include('news.urls')),
 ]
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.http import request
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News
 from main.models import Main
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -33,9 +34,32 @@ def news_add(request):
             error = "All Fields Required"
             return render(request, 'back/error.html', {'error':error})
 
-        b = News(name=newstitle, catname=newscat, catid=0, short_txt=newstxtshort, body_txt=newstxt, date="2020", pic="-", writer="-", show=0)
-        b.save()
-        return redirect('news_list')
+        try:
+            myfile = request.FILES['myfile']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            url = fs.url(filename)
+
+            if str(myfile.content_type).startswith("image"):
+
+                if myfile.size < 5000000 :
+                    b = News(name=newstitle, catname=newscat, catid=0, short_txt=newstxtshort, body_txt=newstxt, date="2020",picname=filename, picurl=url, writer="-", show=0)
+                    b.save()
+                    return redirect('news_list')
+
+                else:
+                    error = "Your Pic Is Bigger Then 5 MB"
+                    return render(request, 'back/error.html', {'error':error})
+
+            else:
+                error = "Your Pic Format is not supported"
+                return render(request, 'back/error.html', {'error':error})
+
+        except:
+            error = "Please Input Your Image"
+            return render(request, 'back/error.html', {'error':error})
+
+        
         
 
 
